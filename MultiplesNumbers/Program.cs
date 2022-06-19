db352f33e110ade23c8c4a281587e10da87b33f3
Corvette31 <corvette.ss@yandex.ru>
2022-06-19 20:11:11 +0300

    Добавьте файлы проекта.

---
+++ MultiplesNumbers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace MultiplesNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int initialRangeValue = 1;
+            int finiteRangeValue = 27;
+            int initialNaturalNumber = 100;
+            int finiteNaturalNumber = 999;
+            int MultiplesNumbers;
+
+            for (int i = initialRangeValue; i <= finiteRangeValue; i++)
+            {
+                MultiplesNumbers = 0;
+
+                for (int j = i; j <= finiteNaturalNumber; j += i)
+                {
+                    if (j >= initialNaturalNumber)
+                    {
+                        MultiplesNumbers++;
+                    }
+                }
+
+                Console.WriteLine($"N = {i} Кратных чисел: {MultiplesNumbers}");
+            }
+        }
+    }
+}
